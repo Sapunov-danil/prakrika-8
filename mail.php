acf23037cd7b0742de33b5124bc51d184e23d682
Sapunov-danil <sapunov.daniil13@gmail.com>
2026-03-06 06:31:18 +0500

    шаг5

--- mail.php
+++ mail.php
@@ -1,23 +1,32 @@
 <?php
 	session_start();
-	session_destroy();
 	include("./settings/connect_datebase.php");
 	
-	if (isset($_SESSION['user'])) {
-		if($_SESSION['user'] != -1) {
-			
-			$user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
-			while($user_read = $user_query->fetch_row()) {
-				if($user_read[3] == 0) header("Location: user.php");
-				else if($user_read[3] == 1) header("Location: admin.php");
-			}
-		}
+    if(isset($_SESSION["user"])) {
+        if($_SESSION['user'] != -1) {
+            $user_query = $mysqli->query("SELECT * FROM `users` WHERE `id` = ".$_SESSION['user']);
+            while($user_read = $user_query->fetch_row()) {
+                if($user_read[3] == 0) header("Location: user.php");
+                else if($user_read[3] == 1) header("Location:admin.php");
+                exit;
+            }
+        }
+    }
+    if (!isset($_SESSION['mail']) || !isset($_SESSION['preuser'])) {
+		header("Location: login.php");
+        exit;
  	}
+
+    $code = rand(100000, 999999);
+    $_SESSION['code'] = $code;
+
+    mail($_SESSION['mail'], 'Код для подтверждения авторизации', 'Код для подтверждения: '.$code);
+    unset($_SESSION['mail']);
 ?>
 <html>
 	<head> 
 		<meta charset="utf-8">
-		<title> Авторизация </title>
+		<title> Подтверждение авторизации </title>
 		
 		<script src="https://code.jquery.com/jquery-1.8.3.js"></script>
 		<link rel="stylesheet" href="style.css">
@@ -36,16 +45,11 @@
 		<div class="main">
 			<div class="content">
 				<div class = "login">
-					<div class="name">Авторизация</div>
+					<div class="name">Подтверждение авторизации</div>
 				
-					<div class = "sub-name">Логин:</div>
-					<input name="_login" type="text" placeholder="" onkeypress="return PressToEnter(event)"/>
-					<div class = "sub-name">Пароль:</div>
-					<input name="_password" type="password" placeholder="" onkeypress="return PressToEnter(event)"/>
-					
-					<a href="regin.php">Регистрация</a>
-					<br><a href="recovery.php">Забыли пароль?</a>
-					<input type="button" class="button" value="Войти" onclick="LogIn()"/>
+					<div class = "sub-name">Код отправленный на почту:</div>
+					<input name="_code" type="text" placeholder="123456" maxlength="6" autocomplete="off" onkeypress="return PressToEnter(event)"/>
+					<input type="button" class="button" value="Войти" onclick="CheckCode()"/>
 					<img src = "img/loading.gif" class="loading"/>
 				</div>
 				
@@ -58,31 +62,34 @@
 		</div>
 		
 		<script>
-			function LogIn() {
-				var loading = document.getElementsByClassName("loading")[0];
-				var button = document.getElementsByClassName("button")[0];
-				
-				var _login = document.getElementsByName("_login")[0].value;
-				var _password = document.getElementsByName("_password")[0].value;
+			function CheckCode() {
+				var codeField = document.getElementsByName("_code")[0];
+                var loading = document.getElementsByClassName("loading")[0];
+                var button = document.getElementsByClassName("button")[0];
+                var _code = codeField.value.trim();
+
+                if(_code.length != 6 || !/^\d{6}$/.test(_code)) {
+                    alert("Введите 6-значный код из письма");
+                    codeField.focus();
+                    return;
+                }
+
 				loading.style.display = "block";
+                button.disabled = true;
 				button.className = "button_diactive";
 				
 				var data = new FormData();
-				data.append("login", _login);
-				data.append("password", _password);
+				data.append("code", _code);
 				
 				// AJAX запрос
 				$.ajax({
-					url         : 'ajax/login_user.php',
+					url         : 'ajax/checkCode.php',
 					type        : 'POST', // важно!
 					data        : data,
 					cache       : false,
 					dataType    : 'html',
-					// отключаем обработку передаваемых данных, пусть передаются как есть
 					processData : false,
-					// отключаем установку заголовка типа запроса. Так jQuery скажет серверу что это строковой запрос
 					contentType : false, 
-					// функция успешного ответа сервера
 					success: function (_data) {
 						console.log("Авторизация прошла успешно, id: " +_data);
 						if(_data == "") {
